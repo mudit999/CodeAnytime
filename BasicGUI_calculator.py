fae90fb80b6bccb7e6e342f9f1e2cc5a72120cbc
Mudit Garg <35555771+mudit999@users.noreply.github.com>
2019-01-20 18:54:37 +0530

    Add files via upload

---
+++ BasicGUI_calculator.py
@@ -0,0 +1,82 @@
+from Tkinter import*
+
+def btnClick(numbers):
+    global operator
+    operator=operator + str(numbers)
+    text_Input.set(operator)
+
+def btnClearDisplay():
+    global operator
+    operator=""
+    text_Input.set("")
+
+def btnEqualsInputs():
+    global operator
+    sumup=str(eval(operator))
+    text_Input.set(sumup)
+    operator=""
+
+
+    
+cal = Tk()
+cal.title("Calculator")
+operator=""
+text_Input =StringVar()
+
+txtDisplay = Entry(cal,font=('Arial',20,'bold'),textvariable=text_Input, bd=20,insertwidth=1,
+                   bg="grey" , justify='right').grid(columnspan=5)
+
+btn7=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="7",command=lambda:btnClick(7)).grid(row=1,column=0)
+
+btn8=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="8",command=lambda:btnClick(8)).grid(row=1,column=1)
+
+btn9=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="9",command=lambda:btnClick(9)).grid(row=1,column=2)
+
+Addition=Button(cal,padx=7,bd=8, fg="Orange",font=('arial',20,'bold'),
+            text="+",command=lambda:btnClick("+")).grid(row=1,column=3)
+#--------------------------------------------------------------------------------------------------
+btn4=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="4",command=lambda:btnClick(4)).grid(row=2,column=0)
+
+btn5=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="5",command=lambda:btnClick(5)).grid(row=2,column=1)
+
+btn6=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="6",command=lambda:btnClick(6)).grid(row=2,column=2)
+
+Subtraction=Button(cal,padx=7,bd=8, fg="Orange",font=('arial',20,'bold'),
+            text="-",command=lambda:btnClick("-")).grid(row=2,column=3)
+#--------------------------------------------------------------------------------------------------
+btn1=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="1",command=lambda:btnClick(1)).grid(row=3,column=0)
+
+btn2=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="2",command=lambda:btnClick(2)).grid(row=3,column=1)
+
+btn3=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="3",command=lambda:btnClick(3)).grid(row=3,column=2)
+
+Division=Button(cal,padx=7,bd=8, fg="Orange",font=('arial',20,'bold'),
+            text="/",command=lambda:btnClick("/")).grid(row=3,column=3)
+#--------------------------------------------------------------------------------------------------
+
+btn0=Button(cal,padx=7,bd=8, fg="black",font=('arial',20,'bold'),
+            text="0",command=lambda:btnClick(0)).grid(row=4,column=0)
+
+Clear=Button(cal,padx=7,bd=8, fg="Orange",font=('arial',20,'bold'),
+            text="C",command=btnClearDisplay).grid(row=4,column=1)
+
+Equals=Button(cal,padx=7,bd=8, fg="Orange",font=('arial',20,'bold'),
+            text="=", command=btnEqualsInputs).grid(row=4,column=2)
+
+Multiply=Button(cal,padx=7,bd=8, fg="Orange",font=('arial',20,'bold'),
+            text="*",command=lambda:btnClick("*")).grid(row=4,column=3)
+
+
+
+
+
+cal.mainloop()
